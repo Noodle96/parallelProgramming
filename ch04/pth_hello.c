6cdac80778df96f45c59d21fe669c39f5c20454b
Noodle96 <jorgealfredomarino2@gmail.com>
2023-11-20 17:25:34 -0500

    pthread_join modificado

--- ch04/pth_hello.c
+++ ch04/pth_hello.c
@@ -1,4 +1,4 @@
-/* File:  
+/* File:
  *    pth_hello.c
  *
  * Purpose:
@@ -17,12 +17,12 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h> 
+#include <pthread.h>
 
 const int MAX_THREADS = 64;
 
 /* Global variable:  accessible to all threads */
-int thread_count;  
+int thread_count;
 
 void Usage(char* prog_name);
 void *Hello(void* rank);  /* Thread function */
@@ -30,23 +30,25 @@ void *Hello(void* rank);  /* Thread function */
 /*--------------------------------------------------------------------*/
 int main(int argc, char* argv[]) {
    // long       thread;  /* Use long in case of a 64-bit system */
-   pthread_t* thread_handles; 
+   pthread_t* thread_handles;
 
    /* Get number of threads from command line */
    if (argc != 2) Usage(argv[0]);
-   thread_count = strtol(argv[1], NULL, 10);  
+   thread_count = strtol(argv[1], NULL, 10);
    if (thread_count <= 0 || thread_count > MAX_THREADS) Usage(argv[0]);
 
-   thread_handles = malloc (thread_count*sizeof(pthread_t)); 
+   thread_handles = malloc (thread_count*sizeof(pthread_t));
    // printf("sizeof %zu\n", sizeof(pthread_t)); // 8
 
-   for (long thread = 0; thread < thread_count; thread++)  
-      pthread_create(&thread_handles[thread], NULL, Hello, (void*) thread);  
+   for (long thread = 0; thread < thread_count; thread++)
+      pthread_create(&thread_handles[thread], NULL, Hello, (void*) thread);
 
    printf("Hello from the main thread\n");
 
-   for (long thread = 0; thread < thread_count; thread++) 
-      pthread_join(thread_handles[thread], NULL); 
+   for (long thread = 0; thread < thread_count; thread++){
+      // printf("we are in joind of %ld\n", thread);
+      pthread_join(thread_handles[thread], NULL);
+   }
 
    free(thread_handles);
    return 0;
@@ -54,7 +56,7 @@ int main(int argc, char* argv[]) {
 
 /*-------------------------------------------------------------------*/
 void *Hello(void* rank) {
-   long my_rank = (long) rank;  /* Use long in case of 64-bit system */ 
+   long my_rank = (long) rank;  /* Use long in case of 64-bit system */
 
    printf("Hello from thread %ld of %d\n", my_rank, thread_count);
 
